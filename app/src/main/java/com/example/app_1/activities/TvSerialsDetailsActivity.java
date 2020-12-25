e8e750bd5dcc9c5ccc46357ea516882d49252df7
Yami-666 <trishin.m.2002@gmail.com>
2020-12-25 14:55:22 +0300

    Added a viewPager2 to the recyclerView items

--- app/src/main/java/com/example/app_1/activities/TvSerialsDetailsActivity.java
+++ app/src/main/java/com/example/app_1/activities/TvSerialsDetailsActivity.java
@@ -1,18 +1,27 @@
     package com.example.app_1.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingComponent;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.app_1.R;
+import com.example.app_1.adapters.ImageSliderAdapter;
 import com.example.app_1.databinding.ActivityTvSerialsDetailsBinding;
 import com.example.app_1.viewmodel.TvSerialsDetailsIViewModel;
 
-public class TvSerialsDetailsActivity extends AppCompatActivity {
+import java.nio.InvalidMarkException;
+
+    public class TvSerialsDetailsActivity extends AppCompatActivity {
 
     private ActivityTvSerialsDetailsBinding activityTvSerialsDetailsBinding;
     private TvSerialsDetailsIViewModel tvSerialsDetailsIViewModel;
@@ -35,8 +44,62 @@ public class TvSerialsDetailsActivity extends AppCompatActivity {
         String tvSerialsId = String.valueOf(getIntent().getIntExtra("id", -1));
         tvSerialsDetailsIViewModel.getTvSerialsDetails(tvSerialsId).observe(this, tvSerialsDetailsResponse -> {
             activityTvSerialsDetailsBinding.setIsLoading(false);
-            Toast.makeText(this, tvSerialsDetailsResponse.getTvSerialsDetails().getUrl(),
-                    Toast.LENGTH_SHORT).show();
+            if (tvSerialsDetailsResponse.getTvSerialsDetails() != null) {
+                if (tvSerialsDetailsResponse.getTvSerialsDetails().getPictures() != null) {
+                    loadImageSlider(tvSerialsDetailsResponse.getTvSerialsDetails().getPictures());
+                }
+            }
         });
     }
+
+    private void loadImageSlider(String[] sliderImages) {
+        activityTvSerialsDetailsBinding.sliderViewPage.setOffscreenPageLimit(1);
+        activityTvSerialsDetailsBinding.sliderViewPage.setAdapter(new ImageSliderAdapter(sliderImages));
+        activityTvSerialsDetailsBinding.sliderViewPage.setVisibility(View.VISIBLE);
+        activityTvSerialsDetailsBinding.viewFadingEdge.setVisibility(View.VISIBLE);
+        setupIndicators(sliderImages.length);
+        activityTvSerialsDetailsBinding.sliderViewPage.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                setupCurrentIndicator(position);
+            }
+        });
+    }
+
+    private void setupIndicators(int count) {
+        ImageView[] indicators = new ImageView[count];
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.setMargins(8,0,8,0);
+        for (int i = 0; i < indicators.length; i++) {
+            indicators[i] = new ImageView(getApplicationContext());
+            indicators[i].setImageDrawable(ContextCompat.getDrawable(
+                    getApplicationContext(),
+                    R.drawable.background_slider_indicator));
+            indicators[i].setLayoutParams(layoutParams);
+            activityTvSerialsDetailsBinding.layoutIndicators.addView(indicators[i]);
+        }
+        activityTvSerialsDetailsBinding.layoutIndicators.setVisibility(View.VISIBLE);
+        setupCurrentIndicator(0);
+    }
+
+    private void setupCurrentIndicator(int position) {
+        int childCount = activityTvSerialsDetailsBinding.layoutIndicators.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            ImageView imageView =
+                    (ImageView) activityTvSerialsDetailsBinding.layoutIndicators.getChildAt(i);
+
+            if (i == position) {
+                imageView.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),
+                        R.drawable.background_slider_indicator_active));
+            } else {
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(),
+                                R.drawable.background_slider_indicator));
+            }
+        }
+    }
 }
+
---
+++ app/src/main/java/com/example/app_1/adapters/ImageSliderAdapter.java
@@ -0,0 +1,58 @@
+package com.example.app_1.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.app_1.R;
+import com.example.app_1.databinding.ItemContainerSliderImageBinding;
+
+public class ImageSliderAdapter extends RecyclerView.Adapter<ImageSliderAdapter.ImageSliderViewHolder> {
+
+    private String[] sliderImage;
+    private LayoutInflater layoutInflater;
+
+    public ImageSliderAdapter(String[] sliderImage) {
+        this.sliderImage = sliderImage;
+    }
+
+
+    @NonNull
+    @Override
+    public ImageSliderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (layoutInflater == null) {
+            layoutInflater = LayoutInflater.from(parent.getContext());
+        }
+        ItemContainerSliderImageBinding sliderImageBinding = DataBindingUtil.inflate(
+                layoutInflater, R.layout.item_container_slider_image, parent, false);
+
+        return new ImageSliderViewHolder(sliderImageBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageSliderViewHolder holder, int position) {
+        holder.bindSliderImage(sliderImage[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return sliderImage.length;
+    }
+
+    static class ImageSliderViewHolder extends RecyclerView.ViewHolder {
+        private ItemContainerSliderImageBinding itemContainerSliderImageBinding;
+
+        public ImageSliderViewHolder(ItemContainerSliderImageBinding itemContainerSliderImageBinding) {
+            super(itemContainerSliderImageBinding.getRoot());
+            this.itemContainerSliderImageBinding = itemContainerSliderImageBinding;
+        }
+
+        public void bindSliderImage(String imageUrl) {
+            itemContainerSliderImageBinding.setImageUrl(imageUrl);
+        }
+    }
+}
