1d2220f4a935fd9ce63e179bd5f6eb761168f2f1
Yami-666 <trishin.m.2002@gmail.com>
2020-12-28 22:14:13 +0300

    Fixed layouts

--- app/src/main/java/com/example/app_1/activities/SignUpActivity.java
+++ app/src/main/java/com/example/app_1/activities/SignUpActivity.java
@@ -1,26 +1,21 @@
-package com.example.app_1.activities;
+ package com.example.app_1.activities;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+ import android.content.Intent;
+ import android.os.Bundle;
+ import android.util.Patterns;
+ import android.view.View;
+ import android.widget.Button;
+ import android.widget.EditText;
+ import android.widget.ProgressBar;
+ import android.widget.TextView;
+ import android.widget.Toast;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Patterns;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
+ import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.app_1.R;
-import com.example.app_1.models.User;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.FirebaseDatabase;
+ import com.example.app_1.R;
+ import com.example.app_1.models.User;
+ import com.google.firebase.auth.FirebaseAuth;
+ import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -107,6 +102,7 @@ public class SignUpActivity extends AppCompatActivity {
                                     }
                                 });
                     } else {
+                        progressBar.setVisibility(View.GONE);
                         Toast.makeText(SignUpActivity.this, "User has be reg " +
                                 "failed! Try again!", Toast.LENGTH_SHORT).show();
                     }
